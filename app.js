8d1523941b744d5149d5e8aa8f66ab824f94173f
Joe Santos <jaysantos012@gmail.com>
2021-07-12 19:53:50 -0400

    adding api call, done with result. now working or display

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup' || 'touchend', (e) => {
     const userText = e.target.value;
 
-    if(userText !== '') {
+    if (userText !== '') {
         // make http call
         github.getUser(userText)
-        .then(data => {
-            if(data.profile.message === 'Not Found') {
-                // show alert
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    // show alert
+                    
 
 
 
+                } else {
+                    // show profile
+                    ui.showProfile(data.profile);
 
-            }else {
-                // show profile
-                ui.showProfile(data.profile);
-           
-            }
-                
+                }
 
-        
-        }) 
+
+
+            })
     } else {
         //clear profile 
 
